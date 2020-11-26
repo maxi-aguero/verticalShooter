9c8b58ef43249953eb4e0fc13ff620016d24ecb7
maxi-aguero <54030746+maxi-aguero@users.noreply.github.com>
2020-11-25 21:37:03 -0300

    Update Personaje.java

--- src/Entidad/Personaje.java
+++ src/Entidad/Personaje.java
@@ -1,28 +1,13 @@
 package Entidad;
 
-
 public abstract class Personaje extends Entidad {
-	//protected double cargaViralActual; //jugador pierde con 100 - comienza con 0									
-	//protected int cargaViralPierde; //infectado pierde con 0- comienza con 100
-	protected int rango; //rango de tiro
-	
 	
 	protected Personaje(int cargaViralActual, int cargaViralPierde, int rango) {
 		super();
 		this.cargaViralActual = cargaViralActual; 
 		this.cargaViralPierde=cargaViralPierde;
-		this.rango = rango;
 				
 	}
-	
-	public int getRango() {	return rango ;}
-	public void setRango(int rango) { this.rango = rango ;}
-	
-	
-
-	
-	
-	
 	
 	
 }
